5f6de37be43e39fe96e79859392d40176d31eb1c
chengchenrui <380101538@qq.com>
2016-08-31 09:23:09 +0800

    添加spring-security

---
+++ src/main/java/org/seckill/util/CreateSession.java
@@ -0,0 +1,13 @@
+/**
+ * Bestpay.com.cn Inc.
+ * Copyright (c) 2011-2016 All Rights Reserved.
+ */
+package org.seckill.util;
+
+/**
+ * @author cheng
+ * @version Id: CreateSession.java, v 0.1 2016/8/31 9:21 cheng Exp $$
+ */
+public class CreateSession {
+    
+}
