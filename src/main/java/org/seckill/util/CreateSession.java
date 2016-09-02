3646088a07a6fd39809a94a7bc32401474fc81a9
chengchenrui <380101538@qq.com>
2016-09-02 09:53:47 +0800

    添加定时任务

--- src/main/java/org/seckill/util/CreateSession.java
+++ src/main/java/org/seckill/util/CreateSession.java
@@ -4,10 +4,32 @@
  */
 package org.seckill.util;
 
+//import org.apache.zookeeper.WatchedEvent;
+//import org.apache.zookeeper.Watcher;
+//import org.apache.zookeeper.ZooKeeper;
+
 /**
  * @author cheng
  * @version Id: CreateSession.java, v 0.1 2016/8/31 9:21 cheng Exp $$
  */
-public class CreateSession {
-    
+public class CreateSession /**implements */ {
+/**
+    private static ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws Exception {
+        zooKeeper = new ZooKeeper("192.168.93.130:2181", 5000, new CreateSession());
+        System.out.println(zooKeeper.getState());
+//        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    public void doSomething() {
+        System.out.println("do something");
+    }
+
+    public void process(WatchedEvent event) {
+        System.out.println("收到事件：" + event.getState());
+        if (event.getState() == Event.KeeperState.SyncConnected) {
+            doSomething();
+        }
+    }*/
 }
---
+++ src/main/java/org/seckill/util/MyElasticJob.java
@@ -0,0 +1,27 @@
+/**
+ * Bestpay.com.cn Inc.
+ * Copyright (c) 2011-2016 All Rights Reserved.
+ */
+package org.seckill.util;
+
+import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author cheng
+ * @version Id: MyElasticJob.java, v 0.1 2016/8/31 16:21 cheng Exp $$
+ */
+@Slf4j
+public class MyElasticJob extends AbstractSimpleElasticJob{
+
+    public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
+        log.info("定时任务开始");
+        try {
+            Thread.sleep(2000);
+            log.info("定时任务结束");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
